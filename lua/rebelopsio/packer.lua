b61791bfb11ac6f4818e63c3f354a210bfb5a1f5
Stephen Morgan <steve@rebelops.io>
2023-04-02 00:40:09 -0400

    feat: more stuff

--- lua/rebelopsio/packer.lua
+++ lua/rebelopsio/packer.lua
@@ -5,7 +5,7 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
   -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+  use {'wbthomason/packer.nvim'}
 
   use {
   'nvim-telescope/telescope.nvim', tag = '0.1.1',
@@ -18,8 +18,31 @@ return require('packer').startup(function(use)
 	config = function()
 		vim.cmd('colorscheme catppuccin')
 	end
-}
+	}
 
 	use({ 'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'}})
+	use {'wakatime/vim-wakatime'}
+	use {'theprimeagen/harpoon'}
+	use {'mbbill/undotree'}
+	use {'tpope/vim-fugitive'}
+	use {
+  'VonHeikemen/lsp-zero.nvim',
+  branch = 'v2.x',
+  requires = {
+    -- LSP Support
+    {'neovim/nvim-lspconfig'},             -- Required
+    {                                      -- Optional
+      'williamboman/mason.nvim',
+      run = function()
+        pcall(vim.cmd, 'MasonUpdate')
+      end,
+    },
+    {'williamboman/mason-lspconfig.nvim'}, -- Optional
 
+    -- Autocompletion
+    {'hrsh7th/nvim-cmp'},     -- Required
+    {'hrsh7th/cmp-nvim-lsp'}, -- Required
+    {'L3MON4D3/LuaSnip'},     -- Required
+  }
+}
 end)
